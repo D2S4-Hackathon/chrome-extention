f81d2127b04be42a74ab67623eeedef32a5a168e
jjjuni <0210leejun@gmail.com>
2025-09-20 20:57:38 +0900

    🔧 setting: 구성 파일 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from 'path'
+import svgr from "vite-plugin-svgr"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      svgrOptions: {
+        icon: true, // SVG를 아이콘처럼 사용 가능
+        // titleProp, ref 등 필요한 옵션 설정 가능
+      },
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'index.html'),
+        background: resolve(__dirname, 'src/background/background.ts'),
+        content: resolve(__dirname, 'src/content/content.ts')
+      },
+      output: {
+        entryFileNames: '[name].js'
+      }
+    },
+    outDir: 'dist',
+  }
 })
