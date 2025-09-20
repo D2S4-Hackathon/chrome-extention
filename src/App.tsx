93b1891b003672e7cec20a83e8c23191181e57e1
jjjuni <0210leejun@gmail.com>
2025-09-21 07:53:06 +0900

    🔧 setting: 세팅 파일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
-import './App.css'
-import Recorder from './component/recoder'
+import Initial from './component/initial/Initial'
+import { useEffect } from 'react'
+import { AnimatePresence } from 'framer-motion'
+import Start from './component/start/Start'
+import useStepStore from './stores/useStepStore'
+import useDocument from './hooks/useDocument'
+import Summary from './component/summary/Summary'
+import useStorageStore from './stores/useStorageStore'
+import Ask from './component/ask/Ask'
+import Answer from './component/answer/Answer'
+
 
 function App() {
 
+  const {
+    step,
+    setStep
+  } = useStepStore()
+
+  const {
+    clear
+  } = useStorageStore();
+
+  useDocument();
+
+  useEffect(() => {
+    clear()
+    setTimeout(() => {
+      setStep('start')
+    }, 1000);
+  }, [])
+
   return (
-    <>
-      <Recorder/>
-    </>
+    <div className={`relative w-[480px] h-[580px] flex flex-col justify-center items-center`}>
+
+      <AnimatePresence mode="wait">
+        {step === 'init' && <Initial key='init' />}
+        {step === 'start' && <Start key='start' />}
+        {step === 'summary' && <Summary key='summary' />}
+        {step === 'ask' && <Ask key='ask' />}
+        {step === 'answer' && <Answer key='answer' />}
+      </AnimatePresence>
+    </div>
   )
 }
-
 export default App
